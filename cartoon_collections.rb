0ad67eb2ebb92f28338ef4902731db41aa39a2e5
Omar Gonzalez <ogonzaleznyc@gmail.com>
2021-02-05 23:39:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,24 +1,48 @@
-def roll_call_dwarves(dwarf_array)
-  dwarf_array.each_with_index do |dwarf_name, i|
-    puts "#{i + 1} #{dwarf_name}"
+# def roll_call_dwarves(dwarf_array)
+#   dwarf_array.each_with_index do |dwarf_name, i|
+#     puts "#{i + 1} #{dwarf_name}"
+#   end
+# end
+
+# def summon_captain_planet(planeteer_calls)
+#   planeteer_calls.map do |call|
+#     "#{call.capitalize}!"
+#   end
+# end
+
+# def long_planeteer_calls(calls)
+#   calls.any? do |call|
+#     call.length > 4
+#   end
+# end
+
+# def find_the_cheese(snacks)
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+#   snacks.find do |snack|
+#     cheese_types.include?(snack) 
+#   end 
+# end
+
+def roll_call_dwarves(array)
+  array.each_with_index do |name, index|
+    puts "#{index+1} #{name}"
   end
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |call|
-    "#{call.capitalize}!"
+def summon_captain_planet(array)
+  array.map do |call|
+    call.capitalize << "!"
   end
 end
 
-def long_planeteer_calls(calls)
-  calls.any? do |call|
-    call.length > 4
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.size > 4
   end
 end
 
-def find_the_cheese(snacks)
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  snacks.find do |snack|
-    cheese_types.include?(snack) 
-  end 
+def find_the_cheese(array)
+  array.find do |item|
+    ["cheddar", "gouda", "camembert"].include?(item)
+  end
 end
